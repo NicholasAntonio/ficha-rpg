be8f97df55403ef8dcba05a3cdf24dd1ada792b2
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-08-24 10:41:18 -0300

    rules download button update

--- src/App.js
+++ src/App.js
@@ -7,36 +7,48 @@ import Skills from "./components/skills/skills";
 import RPGCharacterSheet from "./components/charStatus/charStatus";
 import CharArmor from "./components/charArmor/charArmor";
 import CharEquipment from "./components/charEquipment/charEquipment";
-import {MdOutlineSdStorage} from 'react-icons/md'
-import {GiTec9 } from 'react-icons/gi'
-import {BsPhone} from 'react-icons/bs'
+import { MdOutlineSdStorage } from "react-icons/md";
+import { GiTec9 } from "react-icons/gi";
+import { BsPhone } from "react-icons/bs";
+import {HiDocumentDownload} from "react-icons/hi";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <section>
-        <Logo/>
+        <Logo />
       </section>
       <main>
-      <div className="left">
-        <div className="character-info">
-      <section className="header-and-img">
-        <Header />
-        <ImageUploader />
-        </section>
-        <Attributes />
+        <div className="left">
+          <div className="character-info">
+            <section className="header-and-img">
+              <Header />
+              <ImageUploader />
+            </section>
+            <Attributes />
+          </div>
+          <div className="char-skill">
+            <Skills />
+          </div>
+          <div className="button-container">
+            <a href="/images/regras.odt" download>
+          <Button variant="outlined" color="error" endIcon={<HiDocumentDownload />}>
+            Resumo das regras
+          </Button>
+          </a>
+          </div>
         </div>
-        <div className="char-skill">
-        <Skills />
+        <div className="right">
+          <RPGCharacterSheet />
+          <CharArmor />
+          <CharEquipment title="Equipamentos" icon={<GiTec9 />} />
+          <CharEquipment
+            title="Implantes e hacks"
+            icon={<MdOutlineSdStorage />}
+          />
+          <CharEquipment title="Itens pessoais" icon={<BsPhone />} />
         </div>
-        </div>
-      <div className="right">
-      <RPGCharacterSheet />
-      <CharArmor />
-      <CharEquipment title= 'Equipamentos' icon={<GiTec9 />}/>
-      <CharEquipment title='Implantes e hacks' icon={<MdOutlineSdStorage />}/>
-      <CharEquipment title='Itens pessoais' icon={<BsPhone />}/>
-      </div>
       </main>
     </div>
   );
